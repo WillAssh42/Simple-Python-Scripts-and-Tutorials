298fe711817608b685a24af74e5627cbe26a6b68
ash42 <will.ash420@hotmail.com>
2025-12-09 16:26:06 +0000

    MoneyConverter.py

---
+++ MoneyConverter.py
@@ -0,0 +1,5 @@
+# here were making a more advanced money converter, this is for intermediate/advanced python coders
+print("Here you can convert pounds into dollars") # this prints the text
+pounds = float(input("Input a number to convert: ")) # float converts the text into a floating poinr number with decimals while also printing the input text
+dollars = pounds * 1.33 # this means dollars is equal to pounds
+print("You have", dollars, "dollars") # this prints how many dollars you have 
