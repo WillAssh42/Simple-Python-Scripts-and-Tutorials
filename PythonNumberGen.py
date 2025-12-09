dd0dde4c564359fc83b705fdf56add2d13ffc145
WillAssh42 <will.ash420@hotmail.com>
2025-12-09 10:28:22 +0000

    randomnumberstatements.py

--- PythonNumberGen.py
+++
@@ -1,3 +0,0 @@
-import random
-number = random.randint(1,100) # change 1 or 100 to any number 
-print (number) # prints random number
---
+++ randomnumberstatements.py
@@ -0,0 +1,4 @@
+# this is where you can use the random statement to generate a number, the number can be any number in existance 
+import random # this allows you too use the random.randint(,) statement
+number = random.randint(1,100) # here you can change 1 or 100 to any number 
+print (number) # this prints a random number
