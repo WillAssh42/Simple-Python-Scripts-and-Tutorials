635f495a3414ef30c8ac316a25edc7a4bf943836
ash42 <will.ash420@hotmail.com>
2025-12-10 22:02:04 +0000

    Update Time Statements.py

--- Time Statements.py
+++ Time Statements.py
@@ -1,5 +1,5 @@
 # how to use time statements
-import time # when you import time this allows you to use the wait() command
+import time # when you import this module this allows you to use the wait() command
 # here is an example
 wait(1) # the number = the amount of seconds it waits
 print("hello world!") # this is what happens after the 1 second ( or however long you choose it to be )
